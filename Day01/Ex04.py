43dd520bb34bc6851da89248fbcccae6914ea41e
Pedroxzz <joaopedroicm2117@gmail.com>
2021-12-06 17:40:47 -0300

    first commit

---
+++ Day01/Ex04.py
@@ -0,0 +1,11 @@
+a = input("a: ")
+b = input("b: ")
+
+s = a
+a = b
+b = s
+
+print("a: " + a)
+print("b: " + b)
+
+
---
+++ Day02/Ex01.py
@@ -0,0 +1,5 @@
+two_digit_number = input("Type a two digit number: ")
+n1 = int(two_digit_number[0])
+n2 = int(two_digit_number[1])
+s = n1+n2
+print(s)
